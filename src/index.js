eecb8fe694769a7b04ac772804ac05b7b8aa537c
Faraz <faraz.com>
2023-08-24 02:47:03 +0530

    started adding create service for tweets

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ const express = require('express');
 const connect  = require('./config/database');
 const app = express();
 
-const TweetRepository=require('./repository/tweet-repository');
-const Comment=require('./models/comment');
+const Tweet=require('./models/tweet');
 
 app.listen(3000, async () => {
           console.log('Server started at PORT:3000');
           await connect();
           console.log('Mongo db connected');
+          const tweets=await Tweet.find({
+                    content:["123456","1st mail","34234",'asdff']
+          });
+          console.log(tweets);
 });
