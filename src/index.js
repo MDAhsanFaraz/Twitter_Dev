e278e918d2906cc6036ef4b1ce69f28d333bd7a1
Faraz <faraz.com>
2023-08-23 22:02:42 +0530

    extra code for first commit

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ app.listen(3000, async () => {
           console.log('Mongo db connected');
           
           const tweetrepo=new TweetRepository();
-          const tweet=await tweetrepo.getWithComments('64e5269fea00535e783f8c77');
-          console.log(tweet);
+          const tweet=await tweetrepo.getAll(0,7);
+          console.log(tweet[0].contentWithEmail);
 
 }) 
--- src/repository/tweet-repository.js
+++ src/repository/tweet-repository.js
@@ -1,4 +1,3 @@
-const { response, text } = require('express');
 const Tweet = require('../models/tweet');
 
 class TweetRepository {
@@ -21,16 +20,16 @@ class TweetRepository {
 
     async getWithComments(id) {
         try {
-            const tweet = await Tweet.findById(id).populate({path:'comments'});
+            const tweet = await Tweet.findById(id).populate({ path: 'comments' }).lean();
             return tweet;
         } catch (error) {
             console.log(error);
         }
     }
 
-    async update(tweetId,data) {
+    async update(tweetId, data) {
         try {
-            const tweet = await Tweet.findByIdAndUpdate(tweetId,data,{new:true});
+            const tweet = await Tweet.findByIdAndUpdate(tweetId, data, { new: true });
             return tweet;
         } catch (error) {
             console.log(error);
@@ -45,6 +44,14 @@ class TweetRepository {
             console.log(error);
         }
     }
+    async getAll(offset, limit) {
+        try {
+            const tweet = await Tweet.find().skip(offset).limit(limit);
+            return tweet;
+        } catch (error) {
+            console.log(error);
+        }
+    }
 }
 
-module.exports=TweetRepository;
+module.exports = TweetRepository;
