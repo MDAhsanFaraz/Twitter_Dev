fc2ec37d08f445ecb94830358de053d2f8ba6dd7
Faraz <faraz.com>
2023-08-24 16:45:44 +0530

    moved to es modules

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
-const express = require('express');
-const connect = require('./config/database');
+import express from 'express';
+import {connect} from './config/database.js';
+
+import service from './services/tweet-service.js';
+
 const app = express();
 
 app.listen(3000, async () => {
-	console.log('Server started at PORT:3000');
-	await connect();
-	console.log('Mongo db connected');
-}); 
+    console.log('server started');
+    await connect();
+    console.log('Mongo db connected');
+
+    let ser=new service();
+    await ser.create({content:'this is #Sparta'})
+});
