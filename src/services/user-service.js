70fd3e58a7c2b693ad1410f2e95e8a657832b0e1
Faraz <faraz.com>
2023-08-26 03:01:57 +0530

    Added passport logic

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,12 +1,48 @@
-import{UserRepository} from'../repository/index.js'
-class UserService{
-          constructor()
-          {
-                    this.userRepository=new UserRepository();
-          }
-          async signup(data){
-                    const user =this.userRepository.create(data); 
-                    return user;
-          }
+import e from 'express';
+import { UserRepository } from '../repository/index.js'
+class UserService {
+    constructor() {
+        this.userRepository = new UserRepository();
+    }
+
+    async signup(data) {
+        try {
+            const user = this.userRepository.create(data);
+            return user;
+        } catch (error) {
+            throw error;
+        }
+
+    }
+
+    async getUserByEmail(email) {
+        try {
+            const user = await this.userRepository.findBy({ email });
+            return user;
+        } catch (error) {
+            throw error
+        }
+    }
+    async signin(data) {
+        try {
+            const user = await this.getUserByEmail(data.email);
+            if (!user) {
+                throw{
+                    message: 'no user found',
+                    success: false
+            }
+        }
+            if (!user.comparePassword(data.password)) {
+                throw{
+                    message: 'incorrect password',
+                    success: false
+                };
+            }
+            const token = user.genJWT();
+            return token;
+        } catch (error) {
+            throw error;
+        }
+    }
 }
 export default UserService;
