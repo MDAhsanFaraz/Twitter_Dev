9098428a40cf849ce077bbca26d6ad500da5f477
Faraz <faraz.com>
2023-08-26 00:21:21 +0530

    Added signup with encrypted password

--- src/index.js
+++ src/index.js
@@ -4,9 +4,6 @@ import bodyParser from 'body-parser'
 import apiRoutes from './routes/index.js';
 import {connect} from './config/database.js';
 
-import { UserRepository,TweetRepository } from './repository/index.js';
-import LikeService  from './services/like-service.js';
-
 const app = express();
 
 app.use(bodyParser.json());
@@ -19,13 +16,4 @@ app.listen(3000, async () => {
     console.log('server started');
     await connect();
     console.log('Mongo db connected');
-
-    // const userepo=new UserRepository();
-    // const user=await userepo.getAll();
-
-    // const tweetRepo=new TweetRepository();
-    // const tweet = await tweetRepo.getAll(0,10);
-
-    // const likeService=new LikeService();
-    // await likeService.toggleLike(tweet[0].id,'Tweet',user[0].id);
 });
